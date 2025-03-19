41c2e63ae77efd1d998f8d8f4f639b61870e933d
volard <kirillzhilenkov@gmail.com>
2025-03-20 01:20:25 +0800

    feat(frontend/ui): добавить кнопку перехода в приложение

---
+++ frontend/src/app/open-button/open-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-button',
+  imports: [],
+  templateUrl: './open-button.component.html',
+  styleUrl: './open-button.component.scss'
+})
+export class OpenButtonComponent {
+
+}
