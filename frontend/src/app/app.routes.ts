6ce6c46fc8a04a5ece1a0cdb9db2ac1dca57cc32
volard <kirillzhilenkov@gmail.com>
2025-03-26 15:11:30 +0800

    remove plain frontend

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ShowDataComponent } from './show-data/show-data.component';
-
-export const routes: Routes = [
-    // порядок важен
-    { path: 'show-data', component: ShowDataComponent },
-];
